5a1de8a2935c5f372e428fe8aeb63b1dff443f6a
Henrique Beier <ahoy@harbortype.com>
2019-05-18 12:56:42 -0300

    Added: Delete all layers for the current master

---
+++ Delete all layers for the current master.py
@@ -0,0 +1,34 @@
+#MenuTitle: Delete all layers for the current master
+# -*- coding: utf-8 -*-
+__doc__="""
+Deletes all non-master layers for the current master (including bracket and brace layers) in selected glyphs.
+"""
+
+# Glyphs.showMacroWindow()
+# Glyphs.clearLog()
+
+font = Glyphs.font
+selectedLayers = font.selectedLayers
+
+font.disableUpdateInterface()
+
+for thisLayer in selectedLayers:
+	thisGlyph = thisLayer.parent
+
+	thisGlyph.beginUndo()
+
+	# Build list of layers to be deleted
+	layersToBeDeleted = []
+	for currentLayer in thisGlyph.layers:
+		if currentLayer.associatedMasterId == thisLayer.layerId:
+			if currentLayer.associatedMasterId != currentLayer.layerId:
+				layersToBeDeleted.append(currentLayer.layerId)
+
+	# Delete layers
+	if len(layersToBeDeleted) > 0:
+		for id in layersToBeDeleted:
+			del thisGlyph.layers[id]
+
+	thisGlyph.endUndo()
+
+font.enableUpdateInterface()
