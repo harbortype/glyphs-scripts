165fdb29be0ddca16e245944408109d5586e5ba2
Henrique Beier <ahoy@harbortype.com>
2019-07-08 14:24:00 -0300

    Fix issue that made Duplicate Nodes stop working

--- Duplicate Selected Nodes with Offcurve Points.py
+++ Duplicate Selected Nodes with Offcurve Points.py
@@ -6,42 +6,40 @@ Creates a copy of the selected nodes, adds them in place and create zero-length 
 
 font = Glyphs.font
 layer = font.selectedLayers[0]
+newPathsArray = []
 
 layer.beginChanges()
 
-newPathsArray = []
-
-for pathIndex, path in enumerate( layer.paths ):
-
-    newPath = GSPath()
-
-    for node in path.nodes:
-
-        newPath.addNode_( node.copy() )
-
-        if node.type != "offcurve" and node.selected:
-
-            newPath.nodes[-1].smooth = False
+try:
+    for pathIndex, path in enumerate( layer.paths ):
+        newPath = GSPath()
+        
+        for node in path.nodes:
+            newPath.addNode_( node.copy() )
             
-            offcurveNode1 = GSNode()
-            offcurveNode1.type = "offcurve"
-            offcurveNode1.position = node.position
-            newPath.addNode_( offcurveNode1 )
+            if node.type != "offcurve" and node.selected:
+                newPath.nodes[len(newPath.nodes)-1].smooth = False
+                
+                offcurveNode1 = GSNode()
+                offcurveNode1.type = "offcurve"
+                offcurveNode1.position = node.position
+                newPath.addNode_( offcurveNode1 )
 
-            offcurveNode2 = GSNode()
-            offcurveNode2.type = "offcurve"
-            offcurveNode2.position = node.position
-            newPath.addNode_( offcurveNode2 )
+                offcurveNode2 = GSNode()
+                offcurveNode2.type = "offcurve"
+                offcurveNode2.position = node.position
+                newPath.addNode_( offcurveNode2 )
 
-            newNode = GSNode()
-            newNode.type = "curve"
-            newNode.smooth = False
-            newNode.position = node.position
-            newPath.addNode_( newNode )
-    
-    newPath.closed = True if path.closed else False
-    newPathsArray.append( newPath )
+                newNode = GSNode()
+                newNode.type = "curve"
+                newNode.smooth = False
+                newNode.position = node.position
+                newPath.addNode_( newNode )
+        
+        newPath.closed = True if path.closed else False
+        newPathsArray.append( newPath )
 
-layer.paths = newPathsArray
+    layer.paths = newPathsArray
 
-layer.endChanges()
+finally:
+    layer.endChanges()
--- Duplicate Selected Nodes.py
+++ Duplicate Selected Nodes.py
@@ -7,17 +7,21 @@ Creates a copy of the selected nodes and adds them in place
 font = Glyphs.font
 layer = font.selectedLayers[0]
 
-print layer.selection
+layer.beginChanges()
 
-for path in layer.paths:
+try:
+	for path in layer.paths:
 
-    for node in path.nodes:
+	    for node in path.nodes:
 
-        if node.type != "offcurve" and node.selected:
+	        if node.type != "offcurve" and node.selected:
 
-            newNode = GSNode()
-            newNode.type = "line"
-            newNode.smooth = False
-            newNode.position = node.position
-            node.smooth = False
-            path.insertNode_atIndex_( newNode, node.index+1 )
+	            newNode = GSNode()
+	            newNode.type = "line"
+	            newNode.smooth = False
+	            newNode.position = node.position
+	            node.smooth = False
+	            path.insertNode_atIndex_( newNode, node.index+1 )
+
+finally:
+    layer.endChanges()
