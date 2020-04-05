d3c2288674d51525ec7affd64e818f62d1c940fd
Henrique Beier <ahoy@harbortype.com>
2020-04-05 11:57:20 -0300

    G3 compatibility

--- Add Extremes to Selection.py
+++ Add Extremes to Selection.py
@@ -8,7 +8,20 @@ Adds extreme points to selected paths.
 thisFont = Glyphs.font
 thisLayer = thisFont.selectedLayers[0]
 
-for path in thisLayer.paths:
-	if path.selected:
-		path.addNodesAtExtremes()
-		path.selected = True
+try: 
+    for i in range(len(Layer.shapes)-1,-1,-1):
+        shape = Layer.shapes[i]
+        if type(shape) == GSPath:
+            pathSelected = False
+            for node in shape.nodes:
+                if node in thisLayer.selection:
+                    pathSelected = True
+                    break
+            if pathSelected:
+                shape.addNodesAtExtremes()
+                shape.selected = True
+except:
+    for path in thisLayer.paths:
+        if path.selected:
+            path.addNodesAtExtremes()
+            path.selected = True
--- Duplicate Selected Nodes.py
+++ Duplicate Selected Nodes.py
@@ -9,28 +9,37 @@ font = Glyphs.font
 layer = font.selectedLayers[0]
 newPathsArray = []
 
+def ProcessPath( path ):
+    newPath = GSPath()
+    for node in path.nodes:
+        newPath.addNode_( node.copy() )
+        if node.type != "offcurve" and node.selected:
+            newPath.nodes[len(newPath.nodes)-1].smooth = False
+            newNode = GSNode()
+            newNode.type = "line"
+            newNode.smooth = False
+            newNode.position = node.position
+            newPath.addNode_( newNode )
+    newPath.closed = True if path.closed else False
+    return newPath
+    
 layer.beginChanges()
 
 try:
+    # Glyphs 3
+    for i in range(len(layer.shapes)-1,-1,-1):
+        shape = layer.shapes[i]
+        if isinstance(shape, GSPath):
+            newPath = ProcessPath(shape)
+            newPathsArray.append(newPath)
+        else:
+            newPathsArray.append(shape)
+    layer.shapes = newPathsArray
+except:
+    # Glyphs 2
     for pathIndex, path in enumerate( layer.paths ):
-        newPath = GSPath()
-        
-        for node in path.nodes:
-            newPath.addNode_( node.copy() )
-            
-            if node.type != "offcurve" and node.selected:
-                newPath.nodes[len(newPath.nodes)-1].smooth = False
-
-                newNode = GSNode()
-                newNode.type = "line"
-                newNode.smooth = False
-                newNode.position = node.position
-                newPath.addNode_( newNode )
-        
-        newPath.closed = True if path.closed else False
-        newPathsArray.append( newPath )
-
+        newPath = ProcessPath(path)
+        newPathsArray.append(newPath)
     layer.paths = newPathsArray
-
 finally:
     layer.endChanges()
