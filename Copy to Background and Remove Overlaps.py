cd8555765281709a18a6ba50b64b9799c556cc0d
Henrique Beier <ahoy@harbortype.com>
2020-10-24 12:55:02 +0000

    G3 compatibility

--- Copy to Background and Remove Overlaps.py
+++ Copy to Background and Remove Overlaps.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import division, print_function, unicode_literals
 __doc__="""
-Copies all layers of the selected glyphs to the background, remove overlaps and correct path directions in all layers. Only applies to glyphs with overlaps and skips the ones which do not. Reports in Macro Window.
+Copies all layers of the selected glyphs to the background, remove overlaps and correct path directions in all layers. Skips glyphs with no overlaps. Reports in Macro Window.
 """
 
 def checkForOverlaps( lyr ):
@@ -12,8 +12,11 @@ def checkForOverlaps( lyr ):
 	GSPathOperator = NSClassFromString("GSPathOperator")
 	segments = GSPathOperator.segmentsFromPaths_(paths)
 	count1 = len(segments)
-	PathOperator = GSPathOperator.new()
-	PathOperator.addIntersections_(segments)
+	try: # Glyphs 3
+		GSPathOperator.addIntersections_(segments)		
+	except: # Glyphs 2
+		PathOperator = GSPathOperator.new()
+		PathOperator.addIntersections_(segments)
 	count2 = len(segments)
 	if count1 != count2:
 		return True
@@ -30,6 +33,7 @@ master_ids = [master.id for master in thisFont.masters] # all the master ids
 
 # Glyphs.clearLog() # clears log in Macro window
 thisFont.disableUpdateInterface() # suppresses UI updates in Font View
+text = ""
  
 for thisLayer in selectedLayers:
 	thisGlyph = thisLayer.parent
@@ -39,9 +43,9 @@ for thisLayer in selectedLayers:
 			if layer.layerId in master_ids or layer.isSpecialLayer:
 				print("Processing %s : %s" % (thisGlyph.name, layer.name))
 				process( layer )
+				if "/%s " % (thisGlyph.name) not in text:
+					text += "/%s " % (thisGlyph.name)
 		thisGlyph.endUndo()   # end undo grouping
-	else:
-		print("Skipped %s" % (thisGlyph.name))
 
-
+thisFont.newTab(text)
 thisFont.enableUpdateInterface() # re-enables UI updates in Font View
