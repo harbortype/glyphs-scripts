58b79898105a9ba037e6bea88e2b88c01655e0bf
Henrique Beier <ahoy@harbortype.com>
2020-10-24 11:32:43 -0300

    G3 compatibility

--- Open All Nodes.py
+++ Open All Nodes.py
@@ -8,43 +8,66 @@ Opens all nodes for the selected paths (or all paths if none are selected).
 font = Glyphs.font
 layers = font.selectedLayers
 
+def open_nodes(path):	
+	
+	for segment in path.segments:
+		
+		try: # Glyphs 3
+			countOfPoints = segment.countOfPoints()
+		except: # Glyphs 2
+			countOfPoints = len(segment)
+		
+		newPath = GSPath()
+		if countOfPoints == 4:
+			for n in range(countOfPoints):
+				point = segment[n]
+				newNode = GSNode()
+				newNode.position = point.x, point.y
+				if n == 0:
+					newNode.type = 'line'
+				elif n == 3:
+					newNode.type = 'curve'
+				else:
+					newNode.type = 'offcurve'
+				newPath.addNode_(newNode)
+		else:
+			for n in range(countOfPoints):
+				point = segment[n]
+				newNode = GSNode()
+				newNode.position = point.x, point.y
+				newNode.type = 'line'
+				newPath.addNode_(newNode)
+
+		newPaths.append(newPath)
+
 for layer in layers:
 	
-	layer.beginChanges()
-	allSelected = [ x.parent for x in layer.selection ]
+	try:
+		layer.beginChanges()
+		
+		selection = [ x.parent for x in layer.selection ]
+		selectedPaths = []
+		for item in selection:
+			if item not in selectedPaths and type(item) == GSPath:
+				selectedPaths.append(item)
+		if len(selectedPaths) == 0:
+			selectedPaths = layer.paths
+		
+		newPaths = []
+		try: # Glyphs 3
+			for i in range(len(layer.shapes)-1,-1,-1):
+				shape = layer.shapes[i]
+				if type(shape) == GSPath and shape in selectedPaths:
+					open_nodes(shape)
+					del layer.shapes[i]
+			layer.shapes.extend(newPaths)
+		except: # Glyphs 2
+			for i in range(len(layer.paths)-1,-1,-1):
+				path = layer.paths[i]
+				if path in selectedPaths:
+					open_nodes(path)
+					del layer.paths[i]
+			layer.paths.extend(newPaths)
 	
-	selectedPaths = []
-	for item in allSelected:
-		if item not in selectedPaths and type(item) == GSPath:
-			selectedPaths.append( item )
-
-	if len( selectedPaths ) == 0:
-		selectedPaths = layer.paths
-
-	newPaths = []
-	for path in layer.paths:
-		for segment in path.segments:
-			newPath = GSPath()
-
-			if len(segment) == 4:
-				for nodeIndex, point in enumerate( segment ):
-					newNode = GSNode()
-					newNode.position = point.x, point.y
-					if nodeIndex == 0:
-						newNode.type = 'line'
-					elif nodeIndex == 3:
-						newNode.type = 'curve'
-					else:
-						newNode.type = 'offcurve'
-					newPath.addNode_( newNode )
-			else:
-				for point in segment:
-					newNode = GSNode()
-					newNode.position = point.x, point.y
-					newNode.type = 'line'
-					newPath.addNode_( newNode )
-
-			newPaths.append( newPath )
-
-	layer.paths = newPaths
-	layer.endChanges()
+	finally:
+		layer.endChanges()
--- Open Selected Nodes.py
+++ Open Selected Nodes.py
@@ -8,89 +8,97 @@ Opens the selected nodes. Originally written by Luisa Leitenperger.
 font = Glyphs.font
 layer = font.selectedLayers[0]
 
-layer.beginChanges()
-
-pathArray = [] 
-delArray = []
-
-for pathIndex, path in enumerate( layer.paths ):
+try: 
+    layer.beginChanges()
     
-    # Make a list of selected nodes as long as they're not handles 
-    # (handles can't be split open)
-    selectedNodes = []
-    for node in path.nodes:
-        if node.selected and node.type != "offcurve":
-            selectedNodes.append( node )
+    pathArray = [] 
+    delArray = []
     
-    if len( selectedNodes ) > 0:
-
-        for nodeIndex, node in enumerate( selectedNodes ):
-
-            # Create a new path for each node on the list and 
-            # add copies of every node until the next selected node.
-            newPath = GSPath()
-            
-            # IF PATH IS CLOSED
-            if path.closed:
-
-                # If it's the last (or only) node on the list, 
-                # copy every node until the end of the path then restart 
-                # at the beginning of the path until it reaches the first 
-                # node on the list. 
-                if node == selectedNodes[-1]:
-                    
-                    for newNode in path.nodes[ node.index : ]:
-                        newPath.addNode_( newNode.copy() )
-
-                    # If the first node of the list is also the first node 
-                    # of the path, only that first node will be included, 
-                    # no need for loops.
-                    if selectedNodes[0].index == 0:
-                        newPath.addNode_( path.nodes[0].copy() )
-
-                    else:
-                        for newNode in path.nodes[ 0 : selectedNodes[0].index + 1 ]:
-                            newPath.addNode_( newNode.copy() )
-                    
-                else:
-                    for newNode in path.nodes[ node.index : selectedNodes[nodeIndex + 1].index + 1 ]:
-                        newPath.addNode_( newNode.copy() )
-            
-            # IF PATH IS OPEN
-            else:
-                
-                # Create a path for the first nodes
-                if nodeIndex == 0 and path.nodes[ node.index ].index > 0:
-                    secondPath = GSPath()
-                    for newNode in path.nodes[ : node.index + 1 ]:
-                        secondPath.addNode_( newNode.copy() )
-                    pathArray.append( secondPath )
-
-                # If the node is the last one selected, 
-                # add all nodes until the end of the path
-                if node == selectedNodes[-1]:
-                    for newNode in path.nodes[ node.index : ]:
-                        newPath.addNode_( newNode.copy() )
-
-                # Otherwise, add nodes until the next selected one
-                else:
-                    for newNode in path.nodes[ node.index : selectedNodes[nodeIndex + 1].index + 1 ]:
-                        newPath.addNode_( newNode.copy() )
-
-            # An open path has to start with a line node
-            newPath.nodes[0].type = "line"
-
-            pathArray.append( newPath )
+    for pathIndex, path in enumerate(layer.paths):
         
-        # Flag a path to be deleted if there are selected nodes on that path
-        delArray.append( pathIndex )
+        # Make a list of selected nodes as long as they're not handles 
+        # (handles can't be split open)
+        selectedNodes = []
+        for node in path.nodes:
+            if node.selected and node.type != OFFCURVE:
+                selectedNodes.append(node)
+        
+        if len(selectedNodes) > 0:
+    
+            for nodeIndex, node in enumerate(selectedNodes):
+    
+                # Create a new path for each node on the list and 
+                # add copies of every node until the next selected node.
+                newPath = GSPath()
+                
+                # IF PATH IS CLOSED
+                if path.closed:
+    
+                    # If it's the last (or only) node on the list, 
+                    # copy every node until the end of the path then restart 
+                    # at the beginning of the path until it reaches the first 
+                    # node on the list. 
+                    if node == selectedNodes[-1]:
+                        
+                        for newNode in path.nodes[ node.index : ]:
+                            newPath.addNode_(newNode.copy())
+    
+                        # If the first node of the list is also the first node 
+                        # of the path, only that first node will be included, 
+                        # no need for loops.
+                        if selectedNodes[0].index == 0:
+                            newPath.addNode_(path.nodes[0].copy())
+    
+                        else:
+                            for newNode in path.nodes[ 0 : selectedNodes[0].index + 1 ]:
+                                newPath.addNode_(newNode.copy())
+                        
+                    else:
+                        for newNode in path.nodes[ node.index : selectedNodes[nodeIndex + 1].index + 1 ]:
+                            newPath.addNode_(newNode.copy())
+                
+                # IF PATH IS OPEN
+                else:
+                    
+                    # Create a path for the first nodes
+                    if nodeIndex == 0 and path.nodes[ node.index ].index > 0:
+                        secondPath = GSPath()
+                        for newNode in path.nodes[ : node.index + 1 ]:
+                            secondPath.addNode_(newNode.copy())
+                        pathArray.append(secondPath)
+    
+                    # If the node is the last one selected, 
+                    # add all nodes until the end of the path
+                    if node == selectedNodes[-1]:
+                        for newNode in path.nodes[ node.index : ]:
+                            newPath.addNode_(newNode.copy())
+    
+                    # Otherwise, add nodes until the next selected one
+                    else:
+                        for newNode in path.nodes[ node.index : selectedNodes[nodeIndex + 1].index + 1 ]:
+                            newPath.addNode_(newNode.copy())
+    
+                # An open path has to start with a line node
+                newPath.nodes[0].type = "line"
+    
+                pathArray.append(newPath)
+            
+            # Flag a path to be deleted if there are selected nodes on that path
+            delArray.append(pathIndex)
+    
+    # Delete original paths so there are no duplicates
+    for pathIndex in reversed(delArray):
+        try: # Glyphs 3
+            del(layer.shapes[pathIndex])
+        except: # Glyphs 2
+            del(layer.paths[pathIndex])
+    
+    # Append the open paths
+    for path in pathArray:
+        try: # Glyphs 3
+            layer.shapes.append(path)
+        except: # Glyphs 2
+            layer.paths.append(path)
 
-# Delete original paths so there are no duplicates
-for pathIndex in reversed( delArray ):
-    del( layer.paths[pathIndex] )
-
-# Append the open paths
-for path in pathArray:
-    layer.paths.append( path )
-
-layer.endChanges()
+finally:
+    layer.endChanges()
