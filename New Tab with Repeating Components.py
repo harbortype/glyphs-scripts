f16e74791f11f9d5d635fb661f49635293fefa45
Henrique Beier <ahoy@harbortype.com>
2021-04-08 09:32:59 -0300

    Add check for repeating paths

--- New Tab with Repeating Components.py
+++ New Tab with Repeating Components.py
@@ -10,12 +10,21 @@ txt = ""
 
 for thisGlyph in thisFont.glyphs:
 	firstLayer = thisGlyph.layers[0]
+	# Check for repeating components
 	thisGlyphComponents = []
 	for thisComponent in firstLayer.components:
 		if thisComponent.name in thisGlyphComponents:
 			txt += "/{0}".format(thisGlyph.name)
 			break
 		thisGlyphComponents.append(thisComponent.name)
+	# Check for paths with the same number of nodes in the glyph
+	thisGlyphPaths = []
+	for thisPath in firstLayer.paths:
+		nodeOrder = tuple([node.type for node in thisPath.nodes])
+		if nodeOrder in thisGlyphPaths:
+			txt += "/{0}".format(thisGlyph.name)
+			break
+		thisGlyphPaths.append(nodeOrder)
 
 if txt:
 	Glyphs.font.newTab(txt)
