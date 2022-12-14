1a3ac88d5f33548a8c62ad18fa8af01bca77d537
Henrique Beier <1B450A25-CB348D64-3658691B-BE57F988-9C10DE69>
2022-12-14 08:50:48 -0300

    New script: Report Windows Names

---
+++ Font/Report Windows Names.py
@@ -0,0 +1,44 @@
+#MenuTitle: Report Windows Names
+# -*- coding: utf-8 -*-
+from __future__ import division, print_function, unicode_literals
+__doc__="""
+Checks for the length of nameID 1 and 4, which can cause issues in Word for Windows and Word for Mac respectively.
+"""
+
+Glyphs.clearLog() # clears macro window log
+
+thisFont = Glyphs.font
+
+for thisInstance in thisFont.instances:
+	nameLength = len(thisInstance.windowsFamily)
+	flag = "🆗"
+	delta = ""
+	if nameLength > 31:
+		flag = "⛔️"
+		delta = "({})".format(31 - nameLength)
+	elif nameLength == 31:
+		flag = "⚠️"
+	print("1 {} [{}] {} {}".format(flag, nameLength, thisInstance.windowsFamily, delta))
+
+	nameLength = len(thisInstance.fullName)
+	flag = "🆗"
+	delta = ""
+	if nameLength > 31:
+		flag = "⛔️"
+		delta = "({})".format(31 - nameLength)
+	elif nameLength == 31:
+		flag = "⚠️"
+	print("4 {} [{}] {} {}".format(flag, nameLength, thisInstance.fullName, delta))
+
+	nameLength = len(thisInstance.fontName)
+	flag = "🆗"
+	delta = ""
+	if nameLength > 31:
+		flag = "⛔️"
+		delta = "({})".format(31 - nameLength)
+	elif nameLength == 31:
+		flag = "⚠️"
+	print("6 {} [{}] {} {}".format(flag, nameLength, thisInstance.fontName, delta))
+	print("")
+
+Glyphs.showMacroWindow()
