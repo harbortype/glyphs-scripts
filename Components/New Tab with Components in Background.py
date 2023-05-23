f10f3e3af6ea09f4895ac232d2d9cf8916acb2cc
Henrique Beier <ahoy@harbortype.com>
2023-05-23 08:43:44 -0300

    formatting

--- Components/New Tab with Components in Background.py
+++ Components/New Tab with Components in Background.py
@@ -1,7 +1,8 @@
-#MenuTitle: New Tab with Components in Background
+# MenuTitle: New Tab with Components in Background
 # -*- coding: utf-8 -*-
 from __future__ import division, print_function, unicode_literals
-__doc__="""
+
+__doc__ = """
 Opens a new Edit tab with glyphs containing components in their backgrounds.
 """
 
@@ -9,22 +10,22 @@ thisFont = Glyphs.font
 tabLayers = []
 
 for thisGlyph in thisFont.glyphs:
-	glyphHasComponents = False
-	for thisLayer in thisGlyph.layers:
-		for thisComponent in thisLayer.background.components:
-			tabLayers.append(thisLayer)
-			glyphHasComponents = True
-			break
-	if glyphHasComponents:
-		tabLayers.append(GSControlLayer(10))
+    glyphHasComponents = False
+    for thisLayer in thisGlyph.layers:
+        for thisComponent in thisLayer.background.components:
+            tabLayers.append(thisLayer)
+            glyphHasComponents = True
+            break
+    if glyphHasComponents:
+        tabLayers.append(GSControlLayer(10))
 
 
 if tabLayers:
-	thisFont.newTab()
-	thisFont.tabs[-1].layers = tabLayers
+    thisFont.newTab()
+    thisFont.tabs[-1].layers = tabLayers
 else:
-	Message(
-		title="New Tab with Components in Background",
-		message="There are no components in any glyph background.",
-		OKButton="OK",
-	)
+    Message(
+        title="New Tab with Components in Background",
+        message="There are no components in any glyph background.",
+        OKButton="OK",
+    )
--- Components/New Tab with Nested Components.py
+++ Components/New Tab with Nested Components.py
@@ -1,41 +1,45 @@
-#MenuTitle: New Tab with Nested Components
+# MenuTitle: New Tab with Nested Components
 # -*- coding: utf-8 -*-
 from __future__ import division, print_function, unicode_literals
-__doc__="""
+
+__doc__ = """
 Opens a new Edit tab with glyphs that contain components made of components.
 """
 
 thisFont = Glyphs.font
 txt = ""
 
+
 def hasExportingComponents(glyph):
-	for comp in glyph.layers[0].components:
-		if comp.component.export:
-			return True
-	return False
+    for comp in glyph.layers[0].components:
+        if comp.component.export:
+            return True
+    return False
+
 
 def hasOnlySmartComponents(components):
-	for component in components:
-		if not component.componentName.startswith("_smart."):
-			return False
-	return True
+    for component in components:
+        if not component.componentName.startswith("_smart."):
+            return False
+    return True
+
 
 for thisGlyph in thisFont.glyphs:
-	if not thisGlyph.export:
-		continue
-	firstLayer = thisGlyph.layers[0]
-	for thisComponent in firstLayer.components:
-		otherGlyph = thisFont.glyphs[thisComponent.name]
-		if otherGlyph.layers[0].components:
-			if hasExportingComponents(otherGlyph):
-				txt += "/{0}".format(thisGlyph.name)
-				break
+    if not thisGlyph.export:
+        continue
+    firstLayer = thisGlyph.layers[0]
+    for thisComponent in firstLayer.components:
+        otherGlyph = thisFont.glyphs[thisComponent.name]
+        if otherGlyph.layers[0].components:
+            if hasExportingComponents(otherGlyph):
+                txt += "/{0}".format(thisGlyph.name)
+                break
 
 if txt:
-	Glyphs.font.newTab(txt)
+    Glyphs.font.newTab(txt)
 else:
-	Message(
-		title="New Tab with Nested Components",
-		message="There are no components of components in this font.",
-		OKButton="OK",
-	)
+    Message(
+        title="New Tab with Nested Components",
+        message="There are no components of components in this font.",
+        OKButton="OK",
+    )
