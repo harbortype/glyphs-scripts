cce996b1b2f3f544a7830ddd94c6501c1f70c650
Henrique Beier <ahoy@harbortype.com>
2023-06-12 17:12:11 -0300

    Force integer coordinates

--- Paths/Round Coordinates for Entire Font.py
+++ Paths/Round Coordinates for Entire Font.py
@@ -6,9 +6,19 @@ Round coordinates of all paths in the entire font.
 """
 
 from GlyphsApp import Glyphs
+from Foundation import NSPoint
 
 this_font = Glyphs.font
 
-for this_glyph in this_font.glyphs:
-    for this_layer in this_glyph.layers:
-        this_layer.roundCoordinates()
+try:
+    this_font.disableUpdateInterface()
+    for this_glyph in this_font.glyphs:
+        for this_layer in this_glyph.layers:
+            for this_path in this_layer.paths:
+                for this_node in this_path.nodes:
+                    this_node.position = NSPoint(
+                        int(this_node.position.x),
+                        int(this_node.position.y),
+                    )
+finally:
+    this_font.enableUpdateInterface()
